b3ad2890c0f7373f750eb31d9038f3020ee3d6ab
Unknown <krys.nagorny@gmail.com>
2018-01-12 21:54:17 +0100

    Merge branch 'api-built'
    events location

--- src/routing/index.routing.js
+++ src/routing/index.routing.js
@@ -1,9 +1,11 @@
 import usersRouter from './users.routing';
 import swaggerRoute from './swagger.routes';
 import clickActionRoute from './clickAction.routing';
+import eventRouter from './event.routing';
 
 export {
     usersRouter,
     clickActionRoute,
-    swaggerRoute
+    swaggerRoute,
+    eventRouter
 }
