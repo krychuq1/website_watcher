db43a36a484ead8411c3a4fd5493133de50b272b
Unknown <krys.nagorny@gmail.com>
2018-01-13 22:53:53 +0100

    last buiild

--- src/database/config.js
+++ src/database/config.js
@@ -1,3 +1,3 @@
-const url = 'mongodb://localhost:27017/website_watcher';
-// const url = 'mongodb://websiteWatcher:websiteWatcher123@ds251827.mlab.com:51827/heroku_7l6c835k';
+// const url = 'mongodb://localhost:27017/website_watcher';
+const url = 'mongodb://websiteWatcher:websiteWatcher123@ds251827.mlab.com:51827/heroku_7l6c835k';
 export  default url;
